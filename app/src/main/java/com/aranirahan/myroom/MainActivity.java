9e0131adf93b1f6bb1dd786131dda296bd4ffbe0
Aran Irahan <aranirahan@gmail.com>
2018-07-28 06:37:26 +0700

    Add RoomDatabase

--- app/src/main/java/com/aranirahan/myroom/MainActivity.java
+++ app/src/main/java/com/aranirahan/myroom/MainActivity.java
@@ -3,11 +3,26 @@ package com.aranirahan.myroom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        //set it on background thread
+////        RepoDatabase
+////                .getInstance(context)
+////                .getRepoDao()
+////                .insert(new Repo(1, "Cool Repo Name", "url"));
+//
+//        //lets simple like this
+//        List<Repo> allRepos = RepoDatabase
+//                .getInstance(MainActivity.this)
+//                .getRepoDao()
+//                .getAllRepos();
+
     }
 }
